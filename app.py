ee431a53397c25519637a778ff870a4b58eeb47a
Trịnh Anh Khoa <anhkhoa1612vn@gmail.com>
2026-08-18 08:56:01 +0700

    Set default server port to fixed 8080 in app.py

--- app.py
+++ app.py
@@ -431,4 +431,4 @@ async def export_word(payload: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8086, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
